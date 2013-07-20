fd92c9fcb7c52e4ff5ff22b384ab3b7603babf58
Richard Kettlewell <rjk@terraraq.org.uk>
2013-07-20 09:12:55 +0100

    Bring copyright notices up to date

--- src/rsbackup.cc
+++ src/rsbackup.cc
@@ -1,4 +1,4 @@
-// Copyright © 2011, 2012 Richard Kettlewell.
+// Copyright © 2011-13 Richard Kettlewell.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
