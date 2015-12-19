24458a5b5be88af1611f00eb565cb858571ed560
Richard Kettlewell <rjk@greenend.org.uk>
2015-12-19 14:03:46 +0000

    Idiomatic limits for parseFloat

--- src/Utils.h
+++ src/Utils.h
@@ -25,6 +25,7 @@
 #include <cassert>
 #include <ctime>
 #include <cmath>
+#include <limits>
 
 class IO;                               // forward declaration
 
@@ -95,7 +96,9 @@ int parseInteger(const std::string &s,
  * @throws SyntaxError if the value is out of range
  */
 double parseFloat(const std::string &s,
-                  double min = -HUGE_VAL, double max = HUGE_VAL);
+                  double min = -std::numeric_limits<double>::max(),
+                  double max = std::numeric_limits<double>::max());
+
 
 /** @brief Split and parse a list represented as a string
  * @param bits Destination for components of the string
